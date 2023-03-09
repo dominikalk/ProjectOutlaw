051493160e77dafa5d96eb528113db8b54e44383
Dominik Alkhovik <dominik.alk@hotmail.com>
2023-03-09 04:14:35 +0000

    add start game function

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,30 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 using Unity.Collections.LowLevel.Unsafe;
+using System.Linq;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : NetworkBehaviour
 {
     private float gameLength = 120f;
-    private float gameStartTime;
+    private double gameStartTime;
     private TaskController[] tasks = { };
     [SerializeField] int noOfTasks;
     private int noTasksCompleted = 0;
+    [SerializeField] private Button startGameBtn;
+    private bool startGamePressed = false;
+    [SerializeField] private TextMeshProUGUI text;
 
-    public override void OnNetworkSpawn()
+    public void Start()
+    {
+        Time.timeScale = 0f;
+    }
+
+    public void OnGameStart()
     {
         if (!IsServer) return;
+
+        // Remove tasks to leave set remaining number
         tasks = FindObjectsOfType<TaskController>();
         for (int i = 0; i < tasks.Length - noOfTasks; i++)
         {
-            tasks[i].transform.GetChild(1).GetComponent<NetworkObject>().Despawn();
-            tasks[i].NetworkObject.Despawn();
+            DespawnTaskServerRpc(tasks[i].NetworkObjectId);
         }
-        gameStartTime = Time.time;
+
+        // Start game time
+        gameStartTime = NetworkManager.Singleton.LocalTime.Time;
+        StartGameTimeClientRpc();
+
+        // Set start game button active to false
+        startGamePressed = true;
+        startGameBtn.gameObject.SetActive(false);
     }
 
     private void Update()
     {
-        if (Time.time - gameStartTime >= gameLength) Debug.Log("Sherrifs Win - Time Ran Out");
+        if (!startGamePressed && IsHost) startGameBtn.gameObject.SetActive(true);
+        if (NetworkManager.Singleton.LocalTime.Time - gameStartTime >= gameLength) Debug.Log("Sherrifs Win - Time Ran Out");
     }
 
     [ServerRpc(RequireOwnership = false)]
@@ -34,5 +54,18 @@ public class GameManager : NetworkBehaviour
     {
         noTasksCompleted++;
         if (noTasksCompleted >= noOfTasks) Debug.Log("Outlaws Win - Completed All Tasks");
+    }
+
+    [ServerRpc]
+    private void DespawnTaskServerRpc(ulong taskId)
+    {
+        TaskController taskInRadius = FindObjectsOfType<TaskController>().Where(x => x.NetworkObjectId == taskId).FirstOrDefault();
+        taskInRadius.GetComponent<NetworkObject>().Despawn();
+    }
+
+    [ClientRpc]
+    private void StartGameTimeClientRpc()
+    {
+        Time.timeScale = 1f;
     }
 }
--- Assets/Scripts/NetworkManagerUI.cs
+++ Assets/Scripts/NetworkManagerUI.cs
@@ -6,23 +6,22 @@ using UnityEngine.UI;
 
 public class NetworkManagerUI : MonoBehaviour
 {
-    [SerializeField] private Button serverButton;
     [SerializeField] private Button hostButton;
     [SerializeField] private Button clientButton;
 
     private void Awake()
     {
-        serverButton.onClick.AddListener(() =>
-        {
-            NetworkManager.Singleton.StartServer();
-        });
         hostButton.onClick.AddListener(() =>
         {
             NetworkManager.Singleton.StartHost();
+            hostButton.gameObject.SetActive(false);
+            clientButton.gameObject.SetActive(false);
         });
         clientButton.onClick.AddListener(() =>
         {
             NetworkManager.Singleton.StartClient();
+            hostButton.gameObject.SetActive(false);
+            clientButton.gameObject.SetActive(false);
         });
     }
 }
