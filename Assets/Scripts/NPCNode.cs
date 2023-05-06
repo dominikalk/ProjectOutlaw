71b48d41d2c0d09801e574d920ee058a08b55460
c21090185 <JonesT59@cardiff.ac.uk>
2023-05-06 15:11:07 +0100

    Added NPC movement, NPC prefab, and ListOfNodes

--- Assets/Scripts/NPCNode.cs
+++ Assets/Scripts/NPCNode.cs
@@ -1,10 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NPCNode : MonoBehaviour
 {
-    [SerializeField] public List<NPCNode> adjacentNodes = new List<NPCNode>(); 
+    [SerializeField] public List<NPCNode> adjacentNodes = new List<NPCNode>();
+    [SerializeField] public Boolean isTask;
     // Start is called before the first frame update
     void Start()
     {
