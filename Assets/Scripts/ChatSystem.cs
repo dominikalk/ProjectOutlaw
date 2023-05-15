026bb8250c8cf24cb3966d9bfcf1ae1a5458bdb8
Dominik Alkhovik <dominik.alk@hotmail.com>
2023-05-15 06:00:10 +0100

    fix chat system

--- Assets/Scripts/ChatSystem.cs
+++ Assets/Scripts/ChatSystem.cs
@@ -10,15 +10,12 @@ public class ChatSystem : NetworkBehaviour
     [SerializeField] private TMP_Text chatText;
     [SerializeField] public TMP_InputField chatInput;
 
-    private string playerRole;
     private Dictionary<string, List<string>> chatMessagesByRole = new Dictionary<string, List<string>>();
     private int maxMessagesPerRole = 7;
 
     new public void SendMessage(string role)
     {
         if (string.IsNullOrEmpty(chatInput.text)) return;
-
-        playerRole = role;
 
         string message = $"[{System.DateTime.Now.ToString("hh:mm:ss")}] {chatInput.text}";
 
@@ -42,6 +39,9 @@ public class ChatSystem : NetworkBehaviour
         {
             chatMessagesByRole[role].RemoveAt(0);
         }
+
+        bool isSheriff = NetworkManager.Singleton.LocalClient.PlayerObject.GetComponent<Sheriff>().enabled;
+        string playerRole = isSheriff ? "Sheriff" : "Outlaw";
 
         // Filter messages based on player role
         List<string> filteredMessages = chatMessagesByRole.ContainsKey(playerRole) ? chatMessagesByRole[playerRole] : new List<string>();
